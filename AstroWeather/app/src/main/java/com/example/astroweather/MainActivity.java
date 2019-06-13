c3b41ee3fe7412272b124bea6c49efb688ab6f43
Hubert Liberadzki <209377@edu.p.lodz.pl>
2019-06-14 01:25:43 +0200

    first commit simple layout

--- AstroWeather/app/src/main/java/com/example/astroweather/MainActivity.java
+++ AstroWeather/app/src/main/java/com/example/astroweather/MainActivity.java
@@ -1,9 +1,25 @@
 package com.example.astroweather;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button optionButton;
+
+    private TextView longitudeValue;
+    private TextView latitudeValue;
+    private TextView timeValue;
+    private TextView refreshFrequencyValue;
+
+    private ViewPager viewPager;
+
+    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy.MM.dd\nHH:mm:ss");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ AstroWeather/app/src/main/java/com/example/astroweather/MoonInfo.java
@@ -0,0 +1,4 @@
+package com.example.astroweather;
+
+public class MoonInfo {
+}
